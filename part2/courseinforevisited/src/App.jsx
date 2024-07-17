98ebb8cc763134be3feec924c30890ce0af6614a
Kala-Konarev <kkonarev144@gmail.com>
2024-07-17 10:38:52 +0300

    exercise 2.4

--- part2/courseinforevisited/src/App.jsx
+++ part2/courseinforevisited/src/App.jsx
@@ -31,34 +31,58 @@ const Course = ({ course }) => {
 };
 
 const App = () => {
-    const course = {
-        id: 1,
-        name: "Half Stack application development",
-        parts: [
-            {
-                name: "Fundamentals of React",
-                exercises: 10,
-                id: 1,
-            },
-            {
-                name: "Using props to pass data",
-                exercises: 7,
-                id: 2,
-            },
-            {
-                name: "State of a component",
-                exercises: 14,
-                id: 3,
-            },
-            {
-                name: "New one",
-                exercises: 3,
-                id: 4,
-            },
-        ],
-    };
+    const courses = [
+        {
+            id: 1,
+            name: "Half Stack application development",
+            parts: [
+                {
+                    name: "Fundamentals of React",
+                    exercises: 10,
+                    id: 1,
+                },
+                {
+                    name: "Using props to pass data",
+                    exercises: 7,
+                    id: 2,
+                },
+                {
+                    name: "State of a component",
+                    exercises: 14,
+                    id: 3,
+                },
+                {
+                    name: "New one",
+                    exercises: 3,
+                    id: 4,
+                },
+            ],
+        },
+        {
+            id: 2,
+            name: "New course",
+            parts: [
+                {
+                    name: "Fundamentals",
+                    exercises: 10,
+                    id: 1,
+                },
+                {
+                    name: "Data",
+                    exercises: 7,
+                    id: 2,
+                },
+            ],
+        },
+    ];
 
-    return <Course course={course} />;
+    return (
+        <>
+            {courses.map((c) => (
+                <Course course={c} key={c.id} />
+            ))}
+        </>
+    );
 };
 
 export default App;
