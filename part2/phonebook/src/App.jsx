5eb1f2cf1aa28af48f40a6d91587c72896203f24
Kala-Konarev <kkonarev144@gmail.com>
2024-07-17 12:00:39 +0300

    exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,26 +1,36 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import { useState } from "react";
-const Contact = ({ name }) => {
-    return <p>{name}</p>;
+const Contact = ({ name, number }) => {
+    return (
+        <p>
+            {name} {number}
+        </p>
+    );
 };
 const App = () => {
-    const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+    const [persons, setPersons] = useState([
+        { name: "Arto Hellas", number: 120000 },
+    ]);
     const [newName, setNewName] = useState("");
-
+    const [newNum, setNewNum] = useState("");
     const handleSubmit = (e) => {
         e.preventDefault();
         const exists = persons.find((p) => p.name === newName);
         if (exists) {
             alert(`${newName} is already added to the phonebook`);
         } else {
-            setPersons(persons.concat({ name: newName }));
+            setPersons(persons.concat({ name: newName, number: newNum }));
             setNewName("");
+            setNewNum("");
         }
     };
 
-    const handleChange = (e) => {
+    const handleNameChange = (e) => {
         setNewName(e.target.value);
+    };
+    const handleNumChange = (e) => {
+        setNewNum(e.target.value);
     };
 
     return (
@@ -28,7 +38,16 @@ const App = () => {
             <h2>Phonebook</h2>
             <form onSubmit={handleSubmit}>
                 <div>
-                    name: <input onChange={handleChange} value={newName} />
+                    name:{" "}
+                    <input
+                        required
+                        onChange={handleNameChange}
+                        value={newName}
+                    />
+                </div>
+                <div>
+                    number:{" "}
+                    <input required onChange={handleNumChange} value={newNum} />
                 </div>
                 <div>
                     <button type="submit">add</button>
@@ -36,7 +55,7 @@ const App = () => {
             </form>
             <h2>Numbers</h2>
             {persons.map((p) => (
-                <Contact name={p.name} key={p.name} />
+                <Contact name={p.name} number={p.number} key={p.name} />
             ))}
         </div>
     );
