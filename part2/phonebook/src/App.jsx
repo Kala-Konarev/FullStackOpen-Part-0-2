1263ba1e37bb902e6242fe0ea9e79b004a0f049c
Kala-Konarev <kkonarev144@gmail.com>
2024-07-17 11:44:25 +0300

    exercise 2.6

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+const Contact = ({ name }) => {
+    return <p>{name}</p>;
+};
+const App = () => {
+    const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+    const [newName, setNewName] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setPersons(persons.concat({ name: newName }));
+        setNewName("");
+    };
+
+    const handleChange = (e) => {
+        setNewName(e.target.value);
+    };
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    name: <input onChange={handleChange} value={newName} />
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            {persons.map((p) => (
+                <Contact name={p.name} key={p.name} />
+            ))}
+        </div>
+    );
+};
+
+export default App;
