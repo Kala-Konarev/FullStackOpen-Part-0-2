2d123df6b86497f4b8ab44df97d20fc644ae13da
Kala-Konarev <kkonarev144@gmail.com>
2025-01-23 12:34:13 +0100

    3.12

---
+++ redone/part3/phonebookBackend/mongo.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+if (process.argv.length < 3) {
+    console.log("give password as argument");
+    process.exit(1);
+}
+const password = process.argv[2];
+const url = `mongodb+srv://kkonarev144:${password}@cluster0.s39hj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: Number,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (process.argv.length === 3) {
+    Person.find({}).then((res) => {
+        res.forEach((person) => {
+            console.log(person);
+            mongoose.connection.close();
+        });
+    });
+} else if (process.argv.length === 5) {
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4],
+    });
+
+    person.save().then((res) => {
+        console.log("person saved!");
+        mongoose.connection.close();
+    });
+}
