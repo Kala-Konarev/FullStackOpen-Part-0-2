3272537b02e32e766b9762600678551f5e2a18c5
Kala-Konarev <kkonarev144@gmail.com>
2025-01-24 02:06:27 +0100

    fix regex

--- redone/part3/phonebookBackend/models/person.js
+++ redone/part3/phonebookBackend/models/person.js
@@ -1,13 +1,13 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI;
-mongoose.set("strictQuery", false);
+const url = process.env.MONGODB_URI
+mongoose.set('strictQuery', false)
 mongoose.connect(url).then((result) => {
-    console.log("Connected to ", url);
-});
+    console.log('Connected to ', url)
+})
 
 function numValidator(val) {
-    return /[0-9]{2,3}-[0-9]*/.test(val);
+    return /^\d{2,3}-\d+$/.test(val)
 }
 const personSchema = new mongoose.Schema({
     name: { type: String, minLength: 3, required: true },
@@ -15,19 +15,19 @@ const personSchema = new mongoose.Schema({
         type: String,
         validate: {
             validator: numValidator,
-            message: "Not a valid phone number",
+            message: 'Not a valid phone number',
         },
         minLength: 8,
         required: true,
     },
-});
+})
 
-personSchema.set("toJSON", {
+personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString();
-        delete returnedObject._id;
-        delete returnedObject.__v;
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
     },
-});
+})
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema)
